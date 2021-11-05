5fbecc452b6c2006badeab39b17dea68d36b41bc
Kazapple <467452@student.fontys.nl>
2021-11-05 15:09:34 +0100

    Added Account list + login system linked to database

---
+++ FutureFinanceTest/Controllers/LoginController.cs
@@ -0,0 +1,31 @@
+﻿using FutureFinanceTest.Models;
+using FutureFinanceTest.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FutureFinanceTest.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Processlogin(UserModel userModel)
+        {
+            SecurityService securityService = new SecurityService();
+
+            if(securityService.IsValid(userModel))
+            {
+                return View("LoginSucess", userModel);
+            } else
+            {
+                return View("LoginFailure", userModel);
+            }
+        }
+    }
+}
---
+++ FutureFinanceTest/Services/IAccount.cs
@@ -0,0 +1,15 @@
+﻿using FutureFinanceTest.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FutureFinanceTest.Services
+{
+    interface IAccount
+    {
+        List<AccountModel> GetAccounts();
+        AccountModel GetAccountById(int id);
+        int Update(AccountModel account);
+    }
+}
